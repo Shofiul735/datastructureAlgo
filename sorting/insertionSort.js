54aecb13748703f974c1782c011488dc20e96056
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-11-29 19:35:36 +0600

    #1: insertionSort implemented

---
+++ sorting/insertionSort.js
@@ -0,0 +1,18 @@
+/*
+*   in some cases it's better than selection and bubble sort, like array is almost sorted.
+*/
+
+const insertionSort=(arr)=>{
+    let currVal,i,j;
+    for(i=1;i<arr.length;i++){
+        currVal = arr[i];
+        for(j=i-1;j>=0 && currVal<arr[j];j--){
+            // swap until smaller value than current value
+            arr[j+1]=arr[j];
+        }
+        // j'th is the smaller than current value.
+        arr[j+1]=currVal;
+    }
+    return arr;
+}
+console.log(insertionSort([2,3,4,2,4,6,7,2,1]));
