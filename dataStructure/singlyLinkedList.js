b3b492d09af82f42382460213a9c337c1194f002
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-02 23:07:51 +0600

    #2: push method implemented

---
+++ dataStructure/singlyLinkedList.js
@@ -0,0 +1,30 @@
+/*
+*   Node has a val and next pointer
+*/
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        const node = new Node(val);
+        if(this.head === null){
+            this.head = node;
+            this.tail = node;
+            this.length++;
+            return;
+        }
+        
+        this.tail.next = node;
+        this.tail = node;
+        this.length++;
+    }
+}
