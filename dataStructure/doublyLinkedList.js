bc8c137f61ba6114d937afd875642c4c4210d743
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-03 19:32:23 +0600

    #3: Push method implemented

--- dataStructure/doublyLinkedList.js
+++ dataStructure/doublyLinkedList.js
@@ -12,4 +12,27 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val){
+        let node = new Node(val);
+        if(this.length === 0){
+            this.head = node;
+            this.tail = node;
+        }else{
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return true;
+    }
+
 }
+const list = new DoublyLinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+list.push(4);
+console.log("head:",list.head);
+console.log("_______________________");
+console.log(list.tail);
