cb857e084cc96d818d0d6b48b275568d76de673e
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-04 17:40:34 +0600

    #4: pop method implemented
    This method removes item from the end of the list

--- dataStructure/stack.js
+++ dataStructure/stack.js
@@ -31,5 +31,28 @@ class Stack{
         return true;
     }
 
+    pop(){
+        if(this.length === 0 ) return undefined;
+        let node = this.#tail;
+        this.#tail = this.#tail.prev;
+        node.prev = null;
+        this.length--;
+        return node.value;
+    }
+
     
+}
+
+let stack = new Stack();
+stack.push(1);
+stack.push(5);
+stack.push(3);
+stack.push(-2);
+stack.push(19);
+stack.push(1);
+stack.push(0);
+stack.tail;
+let len = stack.length;
+while(len--){
+    console.log(stack.pop());
 }
