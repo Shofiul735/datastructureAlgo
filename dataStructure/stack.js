6aba0ee300235308504f74441d3cc559c09bb9e1
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-04 17:09:53 +0600

    #4: Node and stack class implemented

---
+++ dataStructure/stack.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.prev = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.tail = null;
+        this.length = 0;
+    }
+    
+}
