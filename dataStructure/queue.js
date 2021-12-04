9632ea6786e904a1303512ffef8684b20830def7
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-04 18:56:49 +0600

    #5: dequeue method implemented
    This method removes item from front of the list

--- dataStructure/queue.js
+++ dataStructure/queue.js
@@ -14,19 +14,34 @@ class Node{
 
 class Queue{
     #head;
+    #tail;
     constructor(){
         this.#head = null;
+        this.#tail = null;
         this.length = 0;
     }
     enqueue(val){
         let node = new Node(val);
         if(this.length === 0){
             this.#head = node;
+            this.#tail = node;
         }else{
-            this.#head.next = node;
-            this.#head = node;
+            this.#tail.next = node;
+            this.#tail = node;
         }
         this.length++;
         return true;
+    }
+    dequeue(){
+        if(this.length === 0) return undefined;
+        let node = this.#head;
+        if(this.length === 1){
+            this.#head = null;   
+        }else{
+            this.#head = node.next;
+            node.next = null;
+        }
+        this.length--;
+        return node.value;
     }
 }
