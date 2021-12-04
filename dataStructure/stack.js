0b17d2bb8c857ff2343af8d7f0eca6b18efe9144
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-04 17:12:04 +0600

    #4: push method implemented
    This method adds item in the end of the list

--- dataStructure/stack.js
+++ dataStructure/stack.js
@@ -10,5 +10,18 @@ class Stack{
         this.tail = null;
         this.length = 0;
     }
+   
     
+    push(val){
+        let node = new Node(val);
+        if(this.length === 0){
+            this.tail = node;
+        }else{
+            node.prev = this.tail;
+            this.tail = node;
+        }
+        this.length++;
+        return true;
+    }
+
 }
