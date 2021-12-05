9010de40714c8c1cd0c7afd139151ee2740458df
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-05 15:08:30 +0600

    #6: insert method is implemented
    This method adds value according to the BST rules

--- dataStructure/tree/binarySearchTree.js
+++ dataStructure/tree/binarySearchTree.js
@@ -1,8 +1,8 @@
 class Node{
     constructor(val){
         this.value = val;
-        this.right = null;
         this.left = null;
+        this.right = null;
     }
 }
 class BinarySearchTree{
@@ -10,4 +10,38 @@ class BinarySearchTree{
     constructor(){
         this.#root = null;
     }
+
+    insert(val){
+        let node = new Node(val)
+        if(this.#root === null){
+            this.#root = node;
+            return true;
+        }else{
+            let rt = this.#root;
+            while(rt!==null){
+                if(val<rt.value){
+                    if(rt.left === null){
+                        rt.left = node;
+                        return true;
+                    }else{
+                        rt = rt.left;
+                    }
+                }else{
+                    if(rt.right === null){
+                        rt.right = node;
+                        return true;
+                    }else{
+                        rt = rt.right
+                    }
+                }
+            }
+        }
+    }
+
 }
+const bst = new BinarySearchTree();
+bst.insert(2);
+bst.insert(1);
+bst.insert(6);
+bst.insert(5);
+console.log(bst);
