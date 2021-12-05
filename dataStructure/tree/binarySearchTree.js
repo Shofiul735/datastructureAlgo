d2fdeffd4a6ab1d1ec843c89e9c92d2277567520
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-05 13:49:12 +0600

    #6: Node and BinarySearchTree class added

---
+++ dataStructure/tree/binarySearchTree.js
@@ -0,0 +1,13 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.right = null;
+        this.left = null;
+    }
+}
+class BinarySearchTree{
+    #root;
+    constructor(){
+        this.#root = null;
+    }
+}
