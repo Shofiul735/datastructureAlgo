22d21da6e13f2951b01ad70cca1b0b4515f35ebc
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-09 00:31:33 +0600

    #8: Max heap rules added in the binaryMaxHeap.js file

---
+++ dataStructure/tree/binaryMaxHeap.js
@@ -0,0 +1,6 @@
+/*
+    Max Heap follows only one rules:
+        Childs must be smaller than the parent.
+        Or, The parent must larger than childs.
+    There is no rules like left has to be smaller or right has to be larger than the parent
+*/
