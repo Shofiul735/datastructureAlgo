fac0550e3b8f8271191eadfdf0aa49cf1d402d46
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-09 01:29:50 +0600

    #8: Parent,child location added in the information section

--- dataStructure/tree/binaryMaxHeap.js
+++ dataStructure/tree/binaryMaxHeap.js
@@ -1,6 +1,9 @@
 /*
-    Max Heap follows only one rules:
+    Max Heap follows only one rule:
         Childs must be smaller than the parent.
         Or, The parent must larger than childs.
     There is no rules like left has to be smaller or right has to be larger than the parent
+
+    In an array left child is stored at 2n+1 and right child is stored at 2n+2.
+    For a child index n, its parent is at (n-1)/2 
 */
