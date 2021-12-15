196fbba74166da130017be350bf4b6978e182481
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-15 18:33:08 +0600

    #9: priority queue implementation finished

--- dataStructure/tree/priorityQueue.js
+++ dataStructure/tree/priorityQueue.js
@@ -4,54 +4,76 @@ class BinaryMinHeap{
         this.#heap = [];
     }
     #bubbleUp(){
-        let childIndex = this.#heap.length-1;
-        let parentIndex = Math.floor((childIndex-1)/2);
-        while(this.#heap[childIndex]<=this.#heap[parentIndex]){
-            [this.#heap[childIndex],this.#heap[parentIndex]] = [this.#heap[parentIndex],this.#heap[childIndex]];
-            childIndex = parentIndex;
-            parentIndex = Math.floor((childIndex-1)/2);
+        if(this.#heap.length>1){
+            let childIndex = this.#heap.length-1;
+            let parentIndex = Math.floor((childIndex-1)/2);
+            while((this.#heap[parentIndex]!== undefined && this.#heap[childIndex] !== undefined) && this.#heap[childIndex].priority < this.#heap[parentIndex].priority){
+                [this.#heap[childIndex],this.#heap[parentIndex]] = [this.#heap[parentIndex],this.#heap[childIndex]];
+                childIndex = parentIndex;
+                parentIndex = Math.floor((childIndex-1)/2);
+            }
         }
-        console.log(this.#heap);
     }
-    insert(value){
-        this.#heap.push(value);
+    enqueue(value,priority){
+        let node = new Node(value,priority);
+        this.#heap.push(node);
         this.#bubbleUp();
     }
 
     #sinkDown(){
-        let parentIndex = 0,childIndex;
-        childIndex = this.#heap[1]>this.#heap[2]? 2:1;
-        while(this.#heap[parentIndex]>=this.#heap[childIndex]){
-            [this.#heap[parentIndex],this.#heap[childIndex]] = [this.#heap[childIndex],this.#heap[parentIndex]];
-            parentIndex = childIndex;
-            childIndex = this.#heap[2*parentIndex+1]>this.#heap[2*parentIndex+2]? 2*parentIndex+2:2*parentIndex+1;
+        if(this.#heap.length > 1){
+            let parentIndex = 0,childIndex;
+            if(this.#heap.length === 2){
+                childIndex = 1;
+            }else{
+                childIndex = this.#heap[1].priority>this.#heap[2].priority ? 2:1;
+            }
+            while(this.#heap[parentIndex].priority>this.#heap[childIndex].priority){
+                [this.#heap[parentIndex],this.#heap[childIndex]] = [this.#heap[childIndex],this.#heap[parentIndex]];
+                parentIndex = childIndex;
+                if(this.#heap[2*parentIndex+1]!== undefined && this.#heap[2*parentIndex+2] !== undefined){
+                    childIndex = this.#heap[2*parentIndex+1].priority>this.#heap[2*parentIndex+2].priority ? 2*parentIndex+2:2*parentIndex+1;
+                }else{
+                    return;
+                }
+            }
         }
     }
 
-    extractMax(){
-        const max = this.#heap[0];
+    dequeue(){
+        const min = this.#heap[0];
         if(this.#heap.length>1){
             this.#heap[0] = this.#heap.pop();
             this.#sinkDown();
         }else{
             this.#heap.pop();
         }
-        return max;
+        return min;
     }
 }
 
+class Node{
+    constructor(value,priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+
 const bmh = new BinaryMinHeap();
-bmh.insert(20);
-bmh.insert(2);
-bmh.insert(28);
-bmh.insert(14);
-bmh.insert(26);
-bmh.insert(12);
-bmh.insert(87);
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
-console.log(bmh.extractMax());
+bmh.enqueue("Izak",100);
+bmh.enqueue("Shofuil",0);
+bmh.enqueue("No-name",10);
+bmh.enqueue("Piku",2);
+bmh.enqueue("Shiku",1);
+bmh.enqueue("Ekri",3);
+bmh.enqueue("Halum",0);
+bmh.enqueue("Meena",15);
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
+console.log(bmh.dequeue());
