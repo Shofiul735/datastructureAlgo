a2f96ab177427c8133525a01ac065097d87e43d9
Md. Shofiul Islam <ytshofiul@gmail.com>
2021-12-16 12:08:48 +0600

    #10: addVertex method implemented
    This method creates an array for every new key of the adjacencyList

--- graph/graph.js
+++ graph/graph.js
@@ -3,4 +3,12 @@ class Graph{
     constructor(){
         this.#adjacencyList = {};
     }
+
+    addVertex(vertex){
+        if(this.#adjacencyList[vertex] === undefined){
+            this.#adjacencyList[vertex] = [];
+            return true;
+        }
+        return false;
+    }
 }
